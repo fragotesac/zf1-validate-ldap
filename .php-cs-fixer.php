346881f63763fbcec9237c96d084e14083982908
Jay Klehr <jay@diablomedia.com>
2021-12-04 15:33:40 -0700

    Merge pull request #138 from diablomedia/php-81
    testing php 8.1

---
+++ .php-cs-fixer.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->files()
+    ->in(__DIR__)
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRiskyAllowed(true)
+    ->setRules(array(
+        '@PSR2'                     => true,
+        'binary_operator_spaces'    => array('operators' => array('=' => 'align', '=>' => 'align')),
+        'single_quote'              => true,
+        'array_syntax'              => array('syntax' => 'long'),
+        'concat_space'              => array('spacing' => 'one'),
+        'psr_autoloading'           => array('dir' => 'src'),
+    ))
+    ->setUsingCache(true)
+    ->setFinder($finder);
+;
